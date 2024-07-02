0220ece2fe8ddb24bf60812fe2a4f7a502d25ede
守田　充希 <>
2024-07-02 19:54:02 +0900

    シードで初期ユーザーのデータを作成

--- AtlasSNS/database/seeds/UsersTableSeeder.php
+++ AtlasSNS/database/seeds/UsersTableSeeder.php
@@ -11,6 +11,16 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('users')->insert([
+            ['username' => 'Atlas一郎',
+            'mail' => 'atlas1@mail.com',
+            'password' => bcrypt('my-secret-password')],
+            ['username' => 'Atlas二郎',
+            'mail' => 'atlas2@mail.com',
+            'password' => bcrypt('my-secret-password')],
+            ['username' => 'Atlas三郎',
+            'mail' => 'atlas3@mail.com',
+            'password' => bcrypt('my-secret-password')]
+        ]);
     }
 }
